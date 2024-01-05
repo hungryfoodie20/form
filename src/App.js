77cc610c7a4655c3c416e91b1b0fb7aa2dfc079f
hungryfoodie20 <hungryfoodie2024@gmail.com>
2024-01-05 20:24:14 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Contact from "./component/contact/Contact"
+import Footer from "./component/footer/Footer"
+
+function App() {
+  return (
+    <div className="App">
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
